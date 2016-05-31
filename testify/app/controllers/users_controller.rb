e72a4d0c044f54f473177b238782f033bdad7927
Ameer Brown <akeithbrown@gmail.com>
2016-05-31 15:29:38 -0700

    create users, bcrypt, new user form, added yeild container.

--- testify/app/controllers/users_controller.rb
+++ testify/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def new
+  @user = User.new
+  render :new
+  end
+
+  def create
+  user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+  @user = User.create(user_params)
+
+  redirect_to index_path
+  end
 end
